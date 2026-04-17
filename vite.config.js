71137de31ce9d3ece3d767dbbd070df22c17c9a8
Victor Blanco <12vblanco@gmail.com>
2026-04-17 16:00:13 +0800

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,11 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    react({
-      // React 19 specific optimizations
-      jsxRuntime: 'automatic',
-      babel: {
-        plugins: [
-          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]
-        ]
-      }
-    })
-  ],
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'gsap', 'framer-motion'],
-    exclude: ['@gsap/react'] // Don't pre-bundle GSAP React plugin
-  },
+  plugins: [react()],
   build: {
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'gsap': ['gsap', '@gsap/react'],
-          'framer': ['framer-motion']
-        }
-      }
-    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'gsap', 'framer-motion', 'lenis'],
   }
 });
