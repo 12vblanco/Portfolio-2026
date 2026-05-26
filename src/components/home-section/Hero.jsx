40c6ed817c9d1c807a7f10fd013a82bdffcc882f
Victor Blanco <12vblanco@gmail.com>
2026-05-26 17:23:23 +0800

    hero subtitle

--- src/components/home-section/Hero.jsx
+++ src/components/home-section/Hero.jsx
@@ -31,8 +31,8 @@ export const Hero = ({ star1Ref, star2Ref, star3Ref }) => {
         <ContentWrapper>
           <LeftColumn>
             <Subtitle ref={line3Ref}>
-              I specialize in <strong>designing and building websites</strong> using modern technologies.
-              I provide <strong>expert Pendo services</strong>, from audit and setup to expert custom guides, analytics &amp; reporting.
+              Edinburgh-based freelance developer specialising in <strong>designing and building websites</strong> using modern technologies.
+              I provide <strong>expert Pendo services</strong>, from audit and setup to custom guides, analytics &amp; reporting.
             </Subtitle>
           </LeftColumn>
 
